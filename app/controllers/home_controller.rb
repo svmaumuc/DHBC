50ca359eefc8d7ea0abc8cf20a8d3cffe91f333d
mygithub <hoangdtvnn@gmail.com>
2014-07-08 09:42:58 +0700

    Thêm kiểm tra kết quả tĩnh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,21 @@ class HomeController < ApplicationController
 
   def play
     @listext = ['A','B','C','B','E','O','H','I','N','E'];
-    @urlimg = "/assets/1.jpg"
-    @result = "BIENBAO"
+    @urlimg = '/assets/1.jpg'
+    $result = 'BIENBAO'
   end
 
   def check
-    @listext = ['O','B','A','B','G','O','G','H','N','U'];
-    @urlimg = "/assets/nguao.jpg"
-    @result = "NGUAO"
+
+    #kiem tra cau hoi cu
+    $kiemtra = false
+    if params['answer'] == $result
+      $kiemtra  = true
+
+      #tạo câu hỏi mới
+      @listext = ['O','B','A','B','G','O','G','H','N','U'];
+      @urlimg = '/assets/nguao.jpg'
+      $result = 'NGUAO'
+    end
   end
 end
